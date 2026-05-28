a35a3f9bc5f7e462a88afc7e66de2721318e7e68
kavindhivya2021-ui <kavindhivya2021@gmail.com>
2026-05-28 20:27:42 +0530

    c

--- dag_1.py
+++ dag_1.py
@@ -20,7 +20,7 @@ with DAG(
     schedule_interval=None,             # "None" means it will only run manually when you click 'Trigger'
     start_date=datetime(2026, 1, 1),    # Start date matching the current timeline
     catchup=False                     # Prevents running historical backfills upon activation
-    tags=['learning']
+    tags=['learning]
 ) as dag:
 
     # 3. Define Task 1: Print a text string
