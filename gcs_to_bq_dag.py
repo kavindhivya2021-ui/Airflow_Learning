c06e62ccc5919d9fccd7a99fdc7232a0efb3d81d
kavindhivya2021-ui <kavindhivya2021@gmail.com>
2026-06-01 09:15:27 +0530

    intentation

--- gcs_to_bq_dag.py
+++ gcs_to_bq_dag.py
@@ -18,19 +18,19 @@ with DAG(
     dag_id='gcs_to_bigquery_pipeline',
     default_args=default_args,
     description='A pipeline to load data from GCS into BigQuery',
-    schedule_interval=None,  # Set a cron schedule here if you want it automated
+    schedule_interval=None,
     catchup=False,
 ) as dag:
 
-   load_csv_to_bq = GCSToBigQueryOperator(
-    task_id='gcs_to_bigquery_task',
-    bucket=' source-bucket-001-learning',
-    source_objects=['customer/customer_data.csv'],
-    destination_project_dataset_table='subtle-anthem-497411-u0.staging.customers',
-    source_format='CSV',
-    skip_leading_rows=1,
-    autodetect=True,
-    write_disposition='WRITE_TRUNCATE',
-)
+    load_csv_to_bq = GCSToBigQueryOperator(
+        task_id='gcs_to_bigquery_task',
+        bucket='source-bucket-001-learning',
+        source_objects=['customer/customer_data.csv'],
+        destination_project_dataset_table='subtle-anthem-497411-u0.staging.customers',
+        source_format='CSV',
+        skip_leading_rows=1,
+        autodetect=True,
+        write_disposition='WRITE_TRUNCATE',
+    )
 
     load_csv_to_bq
