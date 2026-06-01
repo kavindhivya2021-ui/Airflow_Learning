dfcfc571b5fbbf1ad10bc63e874505ab6fe4990b
kavindhivya2021-ui <kavindhivya2021@gmail.com>
2026-06-01 09:19:57 +0530

    h

--- gcs_to_bq_dag.py
+++ gcs_to_bq_dag.py
@@ -25,7 +25,7 @@ with DAG(
     load_csv_to_bq = GCSToBigQueryOperator(
         task_id='gcs_to_bigquery_task',
         bucket='source-bucket-001-learning',
-        source_objects=['customer/customer_data.csv'],
+        source_objects=['customer/customers_data.csv'],
         destination_project_dataset_table='subtle-anthem-497411-u0.staging.customers',
         source_format='CSV',
         skip_leading_rows=1,
